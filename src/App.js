ece8012665a35967010deec1943f44689d87e007
Vaishnavi ravichandran <ravichandran.vaishu@gmail.com>
2020-12-08 12:43:09 -0600

    final

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
 
   const [pageNo,setPageNo] = useState(1)
 
+  
+
  
 
   function handleChange(event){
@@ -28,12 +30,27 @@ function App() {
   },[])
 
   function fetchMoreData(){
+    let url = `https://api.unsplash.com/photos?page=${pageNo}&client_id=wCfpMYaIMhDxhXWfdVSKpvGJHxVEBgFAmX4sqDQEXuU`
 
-    fetch(`https://api.unsplash.com/photos?page=${pageNo}&per_page=20&client_id=wCfpMYaIMhDxhXWfdVSKpvGJHxVEBgFAmX4sqDQEXuU`)
+    if(input){ url =`https://api.unsplash.com/search/photos?page=${pageNo}&query=${input}&client_id=wCfpMYaIMhDxhXWfdVSKpvGJHxVEBgFAmX4sqDQEXuU`}
+   
+    fetch(url)
     .then(response => response.json())
-    .then(data =>   setImageArr(prevArr => [...prevArr,...data]) )
+    .then(data =>  {
 
+      const imagesFromApi = data.results ?? data;
+
+      // if page is 1, then we need a whole new array of images
+      if (pageNo === 1) {
+        setImageArr(imagesFromApi);
+        return;
+      }
+
+      // if page > 1, then we are adding for our infinite scroll
+      setImageArr((images) => [...images, ...imagesFromApi]);
+    })
     setPageNo(prevNo => prevNo+1)
+   
   }
 
 
@@ -41,27 +58,37 @@ function App() {
 
     let randomIndex = 2;     let spanClass=""; let row = 0;
 
+   function map(arr){ 
+     return arr.map((elm,i) => {
+        
+      randomIndex == 2 ? randomIndex = 7 : randomIndex = 2 
+     console.log(row)
+     i%randomIndex == 0 ? spanClass = "span-columns" : spanClass =""
+       return(
+       
+
+       <div className={`images-div ${spanClass}`}>
+        <a href={elm.links.html} target="_blank"> <img src={elm.urls.small} className="images"/> </a>
+       </div>)
+
+
+     }) }
+
     if(imageArr.length !== 0){
 
-       setGalleryDiv(  imageArr.map((elm,i) => {
-        
-        
-         randomIndex == 2 ? randomIndex = 7 : randomIndex = 2 
-        console.log(row)
-        i%randomIndex == 0 ? spanClass = "span-columns" : spanClass =""
-          return(
-          
-
-          <div className={`images-div ${spanClass}`}>
-           <a href={elm.links.html} target="_blank"> <img src={elm.urls.small} className="images"/> </a>
-          </div>)
-
-
-        })
-       )
-
+       setGalleryDiv(map(imageArr))
+      
     }
+    
+
   },[imageArr])
+
+  function handleSearch(e){
+    e.preventDefault();
+    setPageNo(1);
+    fetchMoreData()
+  
+  }
 
  
   
@@ -72,7 +99,7 @@ function App() {
 
      <div className="search-container"> 
      <input type="text" value={input} onChange={handleChange} placeholder="Beautiful Day..." />    
-     <button>Search</button> 
+     <button onClick={handleSearch}>Search</button> 
      </div> 
 
      <InfiniteScroll 
